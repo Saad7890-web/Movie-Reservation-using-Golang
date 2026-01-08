2951913f7b6a19af0c3dcba235c273545d896038
Saad7890-web <omysaadislam@gmail.com>
2026-01-09 02:38:17 +0600

    created middleware structure

---
+++ internal/server/middleware.go
@@ -0,0 +1,14 @@
+package server
+
+import "net/http"
+
+
+type Middleware func(http.Handler)http.Handler
+
+func ApplyMiddleware(h http.Handler, middlewares ...Middleware)http.Handler{
+	for _,m := range middlewares {
+		h = m(h)
+	}
+
+	return h
+}
